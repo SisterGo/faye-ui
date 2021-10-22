bcb9ea3444bae7566343664a368d4061b86cb4e8
MuRui666 <1360643156@qq.com>
2021-10-22 15:55:04 +0800

    add

--- packages/test/index.vue
+++ packages/test/index.vue
@@ -4,7 +4,7 @@
  * @Author: LiuXin
  * @Date: 2021-10-20 00:37:59
  * @LastEditors: LiuXin
- * @LastEditTime: 2021-10-20 13:10:41
+ * @LastEditTime: 2021-10-22 10:04:51
 -->
 <template>
   <h1>我是一个测试组件</h1>
@@ -14,3 +14,8 @@ export default {
   name: 'Test'
 }
 </script>
+<style lang="less" scoped>
+*{
+  margin:0 auto;
+}
+</style>
